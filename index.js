0c53617bef18e53d0acf85afcef5efd956d5c960
cori-peck <cori.peck@ymail.com>
2019-03-18 15:50:22 -0500

    Project start, API is up and running

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // implement your API here
+const express = require('express');
+const server = express();
+
+server.listen(4000, () => {
+    console.log('\n** API up and running on port 4k **');
+});
