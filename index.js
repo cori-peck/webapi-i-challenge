77b9fe2c9708d14d4bf65c52ba2da4c8ebee8836
cori-peck <cori.peck@ymail.com>
2019-03-18 16:43:24 -0500

    Read(find) is implemented.

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // implement your API here
 const express = require('express');
 const server = express();
+const db = require('./data/db');
 
 server.listen(4000, () => {
     console.log('\n** API up and running on port 4k **');
 });
+
+server.get('/api/users', (req, res) => {
+   db
+    .find()
+    .then(users => {
+        res.status(200).json(users);
+    })
+    .catch(error => {
+        res.status(500).json({ error: "The users information could not be retrieved." })
+    })
+})
