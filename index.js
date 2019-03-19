62dc15d38b649527d09d0825517aca9f7d85f9ca
cori-peck <cori.peck@ymail.com>
2019-03-19 15:12:52 -0500

    Create functionality is working

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const server = express();
 const db = require('./data/db');
 
+server.use(express.json());
+
 server.listen(4000, () => {
     console.log('\n** API up and running on port 4k **');
 });
@@ -12,21 +14,25 @@ server.listen(4000, () => {
 server.post('/api/users', (req, res) => {
     console.log(req.body)
     const { name, bio } = req.body;
-    if (!{ name, bio }) {
-        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+    const user = { name, bio }
+    if (!name || !bio) {
+        return res
+        .status(400).json({ errorMessage: "Please provide name and bio for the user." })
     }
-    if({ name, bio}) {
+
         db
-        .insert({ name, bio })
-        .then(res => {
-            res.status(201).json(res)
+        .insert(user)
+        .then(userId => {
+            const { id } = userId;
+
+            db
+            .findById(id)
+            .then(user => {
+                res.status(201).json({ user });
+            })
         })
-        .catch(error => {
-            res.status(500).json({ errorMessage: "There was an error while saving the user to the database."})
-    })} else {
-        res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
-    }
-})
+        .catch(error => res.status(500).json({ error: "There was an errorwhile saving the user to the database."}));
+    })
 
 
 // R - Read
@@ -40,3 +46,6 @@ server.get('/api/users', (req, res) => {
         res.status(500).json({ error: "The users information could not be retrieved." })
     })
 })
+
+
+// U - Update
